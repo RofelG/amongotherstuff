7794f2d6330921c6af60a8c7451edc619820bd2f
Rofel <rofel_p_ganado@hotmail.com>
2022-06-08 04:46:05 +0000

    Init HTML

--- amongotherstuff.js
+++ amongotherstuff.js
@@ -1,6 +1,25 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Worlds\n');
-}).listen(8080, 'localhost');
-console.log('Server running at http://localhost:8080/');
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.send(/public/index.html);
+//   res.end('Hello\n');
+// }).listen(8080, 'localhost');
+// console.log('Server running at http://localhost:8080/');
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+console.log(__dirname);
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 8080);
+
+console.log('Running at Port 8080');
